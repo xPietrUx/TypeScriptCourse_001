d361d6371bacfeee8176bea2913468c2f7c94a9e
Piotr <piotrdusinski01@gmail.com>
2024-01-31 17:07:36 +0100

    Functions in ts

--- TypeScript_001/src/app.ts
+++ TypeScript_001/src/app.ts
@@ -6,3 +6,7 @@ let ageAsString: string = 'twenty';
 ageAsString = `${age}`;
 console.log(ageAsString);
 // age = ageAsString;
+
+const add = (v1: number, v2: number) => v1 + v2;
+
+console.log(add(10, 5));
