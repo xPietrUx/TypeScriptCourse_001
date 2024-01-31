7ae769f2bbedfd036899f0ce4a54ee983d391f7c
Piotr <piotrdusinski01@gmail.com>
2024-01-31 19:45:12 +0100

    Boolean in practice

---
+++ TypeScript_004/src/app.ts
@@ -0,0 +1,13 @@
+const buttonElement = document.querySelector('button');
+
+const calculatePrice = (originalPrice: number, hasDiscount: boolean) => {
+  return hasDiscount ? originalPrice * 0.8 : originalPrice;
+};
+
+buttonElement.addEventListener('click', () => {
+  const originalPrice: number = 50;
+  const hasDiscount: boolean =
+    new URLSearchParams(window.location.search).get('discount') === 'true';
+
+  console.log(calculatePrice(originalPrice, hasDiscount));
+});
