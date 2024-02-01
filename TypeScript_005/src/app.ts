ce93302ce72bb16bb310b73d61267dd0d6428e7f
Piotr <piotrdusinski01@gmail.com>
2024-02-01 15:10:57 +0100

    Modules, imports, exports in TS

--- TypeScript_005/src/app.ts
+++ TypeScript_005/src/app.ts
@@ -2,6 +2,10 @@
 // Gym
 // Feed a cat
 
+import { renderCategories } from './helpers/render-categories-helper.js';
+import { render } from './helpers/render-tasks-helper.js';
+import { Category, Task } from './types/types';
+
 const taskNameInputElement: HTMLInputElement = document.querySelector('#name');
 const addButtonElement: HTMLButtonElement = document.querySelector('button');
 const tasksContainerElement: HTMLElement = document.querySelector('.tasks');
@@ -14,14 +18,6 @@ let selectedCategory: Category;
 //     name: 'Throw out rubbish',
 //     done: false
 // }
-
-type Category = 'general' | 'work' | 'gym' | 'hobby';
-
-interface Task {
-  name: string;
-  done: boolean;
-  category?: Category;
-}
 
 const categories: Category[] = ['general', 'work', 'gym', 'hobby'];
 
@@ -41,64 +37,6 @@ const tasks: Task[] = [
   },
 ];
 
-const render = () => {
-  /* <li>
-        <label for="task-1">Throw out rubbish</label>
-        <input type="checkbox" id="task-1" name="Throw out rubbish" />
-        </li> */
-
-  tasksContainerElement.innerHTML = '';
-  tasks.forEach((task, index) => {
-    const taskElement: HTMLElement = document.createElement('li');
-    if (task.category) {
-      taskElement.classList.add(task.category);
-    }
-    const id: string = `task-${index}`;
-
-    const labelElement: HTMLLabelElement = document.createElement('label');
-    labelElement.innerText = task.name;
-    labelElement.setAttribute('for', id);
-
-    const checkboxElement: HTMLInputElement = document.createElement('input');
-    checkboxElement.type = 'checkbox';
-    checkboxElement.name = task.name;
-    checkboxElement.id = id;
-    checkboxElement.checked = task.done;
-    checkboxElement.addEventListener('change', () => {
-      task.done = !task.done;
-    });
-
-    taskElement.appendChild(labelElement);
-    taskElement.appendChild(checkboxElement);
-
-    tasksContainerElement.appendChild(taskElement);
-  });
-};
-
-const renderCategories = () => {
-  categories.forEach((category) => {
-    const categoryElement: HTMLElement = document.createElement('li');
-
-    const radioInputElement: HTMLInputElement = document.createElement('input');
-    radioInputElement.type = 'radio';
-    radioInputElement.name = 'category';
-    radioInputElement.value = category;
-    radioInputElement.id = `category-${category}`;
-    radioInputElement.addEventListener('change', () => {
-      selectedCategory = category;
-    });
-
-    const labelElement: HTMLLabelElement = document.createElement('label');
-    labelElement.setAttribute('for', `category-${category}`);
-    labelElement.innerText = category;
-
-    categoryElement.appendChild(radioInputElement);
-    categoryElement.appendChild(labelElement);
-
-    categoriesContainerElement.appendChild(categoryElement);
-  });
-};
-
 const addTask = (task: Task) => {
   tasks.push(task);
 };
@@ -115,8 +53,8 @@ addButtonElement.addEventListener('click', (event: Event) => {
     done: false,
     category: selectedCategory,
   });
-  render();
+  render(tasks, tasksContainerElement);
 });
 
-renderCategories();
-render();
+renderCategories(categories, categoriesContainerElement, selectedCategory);
+render(tasks, tasksContainerElement);
---
+++ TypeScript_005/src/helpers/render-categories-helper.ts
@@ -0,0 +1,29 @@
+import { Category } from '../types/types';
+
+export const renderCategories = (
+  categories: Category[],
+  categoriesContainerElement: HTMLElement,
+  selectedCategory: Category
+) => {
+  categories.forEach((category) => {
+    const categoryElement: HTMLElement = document.createElement('li');
+
+    const radioInputElement: HTMLInputElement = document.createElement('input');
+    radioInputElement.type = 'radio';
+    radioInputElement.name = 'category';
+    radioInputElement.value = category;
+    radioInputElement.id = `category-${category}`;
+    radioInputElement.addEventListener('change', () => {
+      selectedCategory = category;
+    });
+
+    const labelElement: HTMLLabelElement = document.createElement('label');
+    labelElement.setAttribute('for', `category-${category}`);
+    labelElement.innerText = category;
+
+    categoryElement.appendChild(radioInputElement);
+    categoryElement.appendChild(labelElement);
+
+    categoriesContainerElement.appendChild(categoryElement);
+  });
+};
